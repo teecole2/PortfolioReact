726c9948c337939c8d7c37589fcb8a59103f49e6
Trisha Cole <tee.cole2@gmail.com>
2020-12-09 18:45:06 -0500

    word change

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,7 +9,7 @@ const About = () => (
                 <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                     <img className="selfiejpg" src={image} alt="trisha" width="250" height="250" />
                     <h2 className="name text-center">Trisha Cole</h2>
-                    <p className="text-white text-center text-about">Full stack web developer, recently earned a certificate in full stack development from the University of Miami, with newly developed skills in HTML, CSS, Bootstrap, JavaScript, jQuery, APIs, GitHub, Heroku, Materialize, JSON, NodeJS, ExpressJS, MySQL, MongoDb, ReactJS, as well as responsive web design. I am a team worker, problem solver, fast learner, organized and creative person, who is looking to bring web designs to life.</p>
+                    <p className="text-white text-center text-about">Full stack web developer, recently earned a certificate in full stack development from the University of Miami, with newly developed skills in HTML, CSS, Bootstrap, JavaScript, jQuery, APIs, GitHub, Heroku, Materialize, JSON, NodeJS, ExpressJS, MySQL, MongoDb, ReactJS, as well as responsive web design. I am a team player, problem solver, fast learner, organized and creative person, who is looking to bring web designs to life.</p>
                 </div>
             </div>
         </div>
